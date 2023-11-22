c7b21d9da779322373e1c1f8fa2bcfbdf30ee87b
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-21 19:45:53 -0500

    finished

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
-}
+    const boundButton = document.getElementById("button");
+       boundButton.addEventListener('click', function() {
+        alert('I was HelloHarlo!');
+      });
+    }
